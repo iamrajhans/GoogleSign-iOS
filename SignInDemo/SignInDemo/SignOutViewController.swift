690e9e2b23aeaed3e40bc7ca9c12f713aad2a40c
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-19 00:41:15 +0530

    added label for username and email id in sign out controller

--- SignInDemo/SignInDemo/SignOutViewController.swift
+++ SignInDemo/SignInDemo/SignOutViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class SignOutViewController: UIViewController ,GIDSignInUIDelegate{
 
+    
+    @IBOutlet weak var userProfileImage: UIImageView!
+    @IBOutlet weak var emailAddress: UILabel!
+    @IBOutlet weak var username: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
+            let user = GIDSignIn.sharedInstance().currentUser
+            emailAddress?.text = user.profile?.email
+            username?.text = user.profile?.name
+            
+//            if let url = NSURL(string: "\(user.profile?.imageURLWithDimension(100) )"){
+//                if let data = NSData(contentsOfURL: url) {
+//                    self.userProfileImage.image = UIImage(data: data)
+//                }
+//            }
+            
+            
+            print("\(user.profile?.name)")
+            print("\(user.profile?.email)")
+            print("\(user.profile?.imageURLWithDimension(100))")
+        }
         // Do any additional setup after loading the view.
+        
     }
     
     
 
+    
+   
     @IBAction func SignOutButton(sender: AnyObject) {
         
         GIDSignIn.sharedInstance().signOut()
